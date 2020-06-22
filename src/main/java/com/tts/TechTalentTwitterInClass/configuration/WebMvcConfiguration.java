47bd75b07581b2e49890ad3ee836a21fc8c00a2c
jhey614 <jordanheyman@yahoo.com>
2020-06-22 10:41:21 -0400

    end day 1 in class

--- src/main/java/com/tts/TechTalentTwitterInClass/configuration/WebMvcConfiguration.java
+++ src/main/java/com/tts/TechTalentTwitterInClass/configuration/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -12,4 +13,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
+    
+    @Configuration
+    public class ThymeleafConfiguration {
+        @Bean
+        public SpringSecurityDialect springSecurityDialect(){
+            return new SpringSecurityDialect();
+        }
+    }
+    
 }
