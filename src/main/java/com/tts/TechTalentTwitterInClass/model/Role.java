c8bbbdb362c39b1cdadb5635181d0c5be9727622
jhey614 <jordanheyman@yahoo.com>
2020-06-22 09:21:12 -0400

    changed to lombock

--- src/main/java/com/tts/TechTalentTwitterInClass/model/Role.java
+++ src/main/java/com/tts/TechTalentTwitterInClass/model/Role.java
@@ -1,7 +1,20 @@
 package com.tts.TechTalentTwitterInClass.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Role {
     @Id
@@ -9,26 +22,4 @@ public class Role {
     @Column(name = "role_id")
     private Long id;
     private String role;
-    
-    public Role() {}
-	public Role(String role) {
-		this.role = role;
-	}
-	
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	public Long getId() {
-		return id;
-	}
-	
-	@Override
-	public String toString() {
-		return "Role [id=" + id + ", role=" + role + "]";
-	}
-    
-    
 }
